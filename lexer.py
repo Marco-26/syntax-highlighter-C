c2a6f76117b4932a13b8b1cb3d78099a647cb2e9
Marco Rafael Simões Costa <markcostah@gmail.com>
2026-01-12 20:19:50 +0000

    refactor: some code optimizations

--- lexer.py
+++ lexer.py
@@ -1,11 +1,13 @@
 # read in text file based on what is received in cli args.
 # parse it to get the tokens
 # based on token types assign a color
+# maybe add state machine to know if we are in a string, for example
 
 import argparse
 import os
 import sys
 from enum import Enum
+from dataclasses import dataclass
 
 parser = argparse.ArgumentParser(prog="C Syntax Highlighter")
 parser.add_argument("filepath")
@@ -19,17 +21,36 @@ class TokenType(Enum):
   STRING_LITERAL = "string_literal", # everything inside double quote ""
   CHAR = "char" # everything inside ''
   COMMENT = "commment" # everything followed by // or /* */
-  OP = "operators" # ( ) + -
+  OP = "operators" # + -
   WHITESPACE = "whitespace" # /n
+  PUNCTUATORS = "punctuators" # ; ( ) [ ] { }
+  UNKNOWN = "unknown" # unknown tokens
   
+@dataclass(frozen=True, slots=True)
 class Token():
   token_type: TokenType
   token_value: str
+  starting_pos: int
+  ending_pos: int
+  
+type TokenList = list[Token]
 
-def parse_code(content: str) -> list[tuple[TokenType, int, int]]:
+def add_token_to_list(token_list: TokenList, new_token: Token):
+  token_list.append(new_token)
+  
+def reset_variables(initial_index: int, current_index: int, current_token:str):
+  initial_index = current_index
+  current_token = ""
+  current_index += 1
+  
+
+def parse_code(content: str) -> TokenList:
   c_types = {"int", "float", "char"}
   c_identifiers = {"main"}
-  tokens: list[tuple[TokenType, int, int]] = []
+  c_punctuators = {'{','}', '(', ')', '[', ']', ';'}
+  c_keywords = {'return'}
+  
+  tokens: TokenList = []
   
   current_token: str = ""
   content_length = len(content)
@@ -40,13 +61,11 @@ def parse_code(content: str) -> list[tuple[TokenType, int, int]]:
   while current_index < content_length:
     c = content[current_index]
     current_token += c
-    print(current_token, initial_index, current_index)
     
     if c.isspace():
       if current_token[0] == '"':
         current_index += 1
         continue
-      
       current_token = ""
       current_index += 1
       continue
@@ -55,20 +74,27 @@ def parse_code(content: str) -> list[tuple[TokenType, int, int]]:
       # detect if this is a start of new word
       initial_index = current_index
     
-    if current_token in c_types:
-      tokens.append((TokenType.TYPE, initial_index, current_index))
-      initial_index = current_index
-      current_token = ""
-      current_index += 1
-      continue
-      
-    if current_token in c_identifiers:
-      tokens.append((TokenType.IDENT, initial_index, current_index))
-      initial_index = current_index
-      current_token = ""
-      current_index += 1
-      continue
-    
+    match current_token:
+      case _ if current_token in c_types:
+        add_token_to_list(token_list=tokens, new_token=Token(TokenType.TYPE, current_token, initial_index, current_index))
+        reset_variables(initial_index, current_index, current_token)
+        continue
+      case _ if current_token in c_identifiers:
+        add_token_to_list(token_list=tokens, new_token=Token(TokenType.IDENT, current_token, initial_index, current_index))
+        reset_variables(initial_index, current_index, current_token)
+        continue
+      case _ if current_token in c_punctuators:
+        add_token_to_list(token_list=tokens, new_token=Token(TokenType.PUNCTUATORS, current_token, initial_index, current_index))
+        reset_variables(initial_index, current_index, current_token)
+        continue
+      case _ if current_token in c_keywords:
+        add_token_to_list(token_list=tokens, new_token=Token(TokenType.OP, current_token, initial_index, current_index))
+        reset_variables(initial_index, current_index, current_token)
+        continue
+      case _:
+        current_index += 1
+        continue
+        
     current_index += 1
     
   return tokens
@@ -86,4 +112,4 @@ if __name__ == "__main__":
     
   content = read_file_content(filepath=filepath)
   tokens = parse_code(content=content)
-  print(tokens)
+  print(len(tokens), tokens)
